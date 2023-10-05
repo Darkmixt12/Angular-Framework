6127432ebf54189c04b4349883e507889ca8bc3b
Vicious <137929047+Darkmixt12@users.noreply.github.com>
2023-10-04 21:56:45 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import './style.css' //! Import es requerir un modulo, un modulo no es mas que una agrupacion encapsulada de un procedimiento //
 // import './topics/01-basic-types' este es el import para que funcione el 01-basic-types.ts //
-//import './topics/02-object-interface' 
-//import './topics/03-functions' 
-//import './topics/04-homework-types'
-//import './topics/05-basic-destructuring'
+//import './topics/02-object-interface'// 
+//import './topics/03-functions' //
+//import './topics/04-homework-types'///
+//import './topics/05-basic-destructuring'//
 import './topics/06-function-destructuring'
 import './topics/07-import-exports'
 
